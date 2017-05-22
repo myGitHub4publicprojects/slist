2c2dd20e35388d7b43f1709c4631b0118a4ba8b1
kuba <jszczep@gmail.com>
2017-05-22 06:39:38 +0200

    db

--- slist/settings.py
+++ slist/settings.py
@@ -84,6 +84,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'slist.wsgi.application'
 
 
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
