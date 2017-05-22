f82e7944453a04a77100e319d7a7ab50d0d67314
kuba <jszczep@gmail.com>
2017-05-22 06:36:29 +0200

    db settings

--- slist/settings.py
+++ slist/settings.py
@@ -84,16 +84,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'slist.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -138,6 +128,15 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 SIGNUP_REDIRECT = '/'
+
+
+# for heroku
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
 
 try:
     from .local_settings import *
