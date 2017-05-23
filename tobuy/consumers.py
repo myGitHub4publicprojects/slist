78c3cc674660caf0ba910299886722f6e574ea71
myGitHub4publicprojects <jszczep@gmail.com>
2017-05-23 07:46:44 +0200

    debugging heroku

--- tobuy/consumers.py
+++ tobuy/consumers.py
@@ -7,6 +7,13 @@ from .models import Item
 # Connected to websocket.connect
 @channel_session_user_from_http
 def ws_add(message):
+    
+    import sys
+
+    print 'dotarlem do consumers ws_add'
+    sys.stdout.flush()
+
+
     # Accept the connection
     message.reply_channel.send({"accept": True})
     # Add to the users group
